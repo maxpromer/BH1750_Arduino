863f150907a60e0aab315a916688610bc11a18e1
Max <max30012540@hotmail.com>
2020-04-10 20:16:55 +0700

    Update blocks_bh1750.js

--- blocks/blocks_bh1750.js
+++ blocks/blocks_bh1750.js
@@ -6,18 +6,18 @@ Blockly.Blocks['bh1750_begin'] = {
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setColour(135);
-		this.setTooltip("");
-		this.setHelpUrl("");
+		this.setTooltip("Config BH1750 to Continuously H-Resolution Mode and setup I2C");
+		this.setHelpUrl("https://github.com/maxpromer/BH1750_Arduino");
 	}
 };
 
 Blockly.Blocks['bh1750_read'] = {
 	init: function() {
 		this.appendDummyInput()
-			.appendField("BH1750 read (lx)");
+			.appendField("BH1750 read light level (lx)");
 		this.setOutput(true, null);
 		this.setColour(135);
-		this.setTooltip("");
-		this.setHelpUrl("");
+		this.setTooltip("Read and Masurement data from BH1750");
+		this.setHelpUrl("https://github.com/maxpromer/BH1750_Arduino");
 	}
 };
